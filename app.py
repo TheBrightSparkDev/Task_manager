5ac5e2c7f4e10e0e74cd73557323c3c8805522da
TheBrightSparkDev <danharriesxd@gmail.com>
2022-02-23 23:04:40 +0000

    feat - fully functioning registration function added

--- app.py
+++ app.py
@@ -25,8 +25,26 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
-@app.route("/register", methods=["GET","POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if usernmae in form element already exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the newly created user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("registration successful")
+
     return render_template("register.html")
 
 
